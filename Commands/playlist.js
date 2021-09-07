4469d2143aab011a000b36f84a0b18939295f793
TeemoTimeCodes <82971070+TeemoTimeCodes@users.noreply.github.com>
2021-09-07 18:23:47 +0100

    Added Teddy's Playlist

--- Commands/playlist.js
+++ Commands/playlist.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'playlist',
     description: "Spotify",
     execute(message, args, client, Discord){
-        message.channel.send('https://open.spotify.com/playlist/2tSRNLODcbs2Zgm3uJrkXs?si=f3bfe2b2b6de4390')  
+        message.channel.send('https://open.spotify.com/playlist/2tSRNLODcbs2Zgm3uJrkXs?si=f3bfe2b2b6de4390\nhttps://open.spotify.com/playlist/7zxZBpdYBlpUiySOnjesbB?si=1c1d207eee0a440c')  
     }
 }
