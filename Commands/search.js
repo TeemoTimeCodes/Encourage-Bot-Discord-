f3277c6c7f07f76003ad7d673514469a5415347f
TeemoTimeCodes <82971070+TeemoTimeCodes@users.noreply.github.com>
2021-09-07 18:11:31 +0100

    Important fix to search command

--- Commands/search.js
+++ Commands/search.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'search',
     description: "Why?",
     execute(message, args){
-        message.channel.send('https://www.google.co.uk/chrome/?brand=FHFK&gclid=Cj0KCQjwm9yJBhDTARIsABKIcGY1YjRDimf6968kJoxBuA0nzBzUtEsPx5JuclsM_yse6Jw6tcqXM5YaArlDEALw_wcB&gclsrc=aw.ds\nDo it yourself smh.')  
+        message.channel.send('https://www.microsoft.com/en-gb/download/details.aspx?id=41628\nDo it yourself smh.')  
     }
 }
